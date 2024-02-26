793dd61300e078828e57080084c856d7d34c7742
Mrov9072 <mrov2709@gmail.com>
2024-02-26 04:06:17 +0300

    Add some experiments

--- data_download.py
+++ data_download.py
@@ -2,20 +2,11 @@ from ucimlrepo import fetch_ucirepo
 
 def download_data(filename:str)->tuple:
     """
-    Script for downloading data from https://archive.ics.uci.edu/dataset page
+    Script for downloading data from https://archive.ics.uci.edu page
     filename = wine: for wine dataset
-    filename = Convertype: for contvertype dataset
     """
     if filename == "wine":
         wine = fetch_ucirepo(id=109)
-        return (wine.data.features,wine.data.targets)
-    if filename == "Contraceptive":
-        contraceptive_method_choice = fetch_ucirepo(id=30) 
-        return (contraceptive_method_choice.data.featues,contraceptive_method_choice.data.targets)
-    if filename == "convertype":
-        covertype = fetch_ucirepo(id=31) 
-        return (covertype.data.features,covertype.data.targets)
+        return wine.data.features,wine.data.targets
     else:
         print(Exception("Unknown name"))
-    
-
