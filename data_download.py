3e971a719c747aca7ac5cc2ad3f1684c8dbecd8c
fokrey <67243838+fokrey@users.noreply.github.com>
2024-03-16 02:16:12 +0300

    code for downloading data

--- data_download.py
+++ data_download.py
@@ -1,12 +1,41 @@
 from ucimlrepo import fetch_ucirepo
+from sklearn.datasets import fetch_openml
 
-def download_data(filename:str)->tuple:
-    """
-    Script for downloading data from https://archive.ics.uci.edu page
-    filename = wine: for wine dataset
-    """
-    if filename == "wine":
-        wine = fetch_ucirepo(id=109)
-        return wine.data.features,wine.data.targets
+dataset_names_uci = [
+    "Wine", "Contraceptive", "Pen-Based",
+    "Dermatology", "Balance Scale",
+    "Glass", "Heart (Cleveland)", "Car Evaluation",
+    "Yeast", "Shuttle"
+]
+
+dataset_names_openML = [
+    "New Thyroid", "FARS", 'Hayes-Roth', 'Vertebra Column', 'Page blocks'
+]
+
+ids_openML = [
+    40682, 40672, 329, 1523, 30
+]
+
+ids_uci = [
+    109, 30, 81,
+    33, 12,
+    42, 45, 19,
+    110, 148
+]
+
+datasets_uci_dict = dict(zip(dataset_names_uci, ids_uci))
+datasets_openML_dict = dict(zip(dataset_names_openML, ids_openML))
+
+
+def download_data(dataset_name)->tuple:
+    if dataset_name in datasets_uci_dict:
+        dataset = fetch_ucirepo(id=datasets_uci_dict[dataset_name])
+        X, y = dataset.data.features, dataset.data.targets
+
+    elif dataset_name in datasets_openML_dict:
+        dataset = fetch_openml(data_id=datasets_openML_dict[dataset_name], as_frame=True)
+        X, y = dataset.data, dataset.target
+
     else:
-        print(Exception("Unknown name"))
+        raise ValueError("Dataset not found in UCI or OpenML dictionaries.")
+    return X, y
